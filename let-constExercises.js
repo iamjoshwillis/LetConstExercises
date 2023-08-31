d6367bc44aa69b45ae69cbe11fb9810d6dab8dd0
Josh Willis <josh@infinitysoundfactory.com>
2023-08-31 08:39:25 -0400

    initial add

---
+++ let-constExercises.js
@@ -0,0 +1,15 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+/* Write an ES2015 Version */
+const PI = 3.14
+
+
+
+// var offers the ability to redeclare and reassign values, and let creates a block-scoped variable that only exists within a code block. let is also restricted from redeclaring a value, while this is permitted with var.
+
+// while var provides the ability to redeclare and reassign values, const cannot be modified or changed after its declaration
+
+// while neither let or const can be redeclared, only let can be reassigned later on. 
+
+// hoisting is when a newly declared value is moved to the top of the function/region of code they are declared in. This allows it to be valid before it's run later on.
